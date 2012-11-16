70eae445c5b92039383a1d1c66b383c2cdf77d0a
cewing <cris@crisewing.com>
2012-11-16 15:56:14 -0800

    * pep8 fixes for imports  * improve wording for template description text  * default GS Profile to true and hide the question from the 'easy' set.

--- src/templer/dexterity/dexterity.py
+++ src/templer/dexterity/dexterity.py
@@ -1,7 +1,10 @@
 import copy
 
-from templer.zope import BasicZope
-from templer.core.vars import StringVar, EASY, EXPERT
+from templer.plone import Plone
+from templer.core.base import get_var
+from templer.core.vars import StringVar
+from templer.core.vars import EASY
+from templer.core.vars import EXPERT
 
 try:
     from templer.localcommands import SUPPORTS_LOCAL_COMMANDS
@@ -19,12 +22,12 @@ POST_RUN_TEXT = """"""
 if SUPPORTS_LOCAL_COMMANDS:
     HELP_TEXT += """
 This template supports local commands.  These commands allow you to
-generate skeleton content types and behaviors to your new package.
+generate skeleton content types and add behaviors to your new package.
 """
     POST_RUN_TEXT = LOCAL_COMMANDS_MESSAGE
 
 
-class Dexterity(BasicZope):
+class Dexterity(Plone):
     _template_dir = 'templates/dexterity'
     summary = 'A Plone project that uses Dexterity content types'
     help = HELP_TEXT
@@ -35,7 +38,7 @@ class Dexterity(BasicZope):
     use_cheetah = True
     use_local_commands = SUPPORTS_LOCAL_COMMANDS
 
-    vars = copy.deepcopy(BasicZope.vars)
+    vars = copy.deepcopy(Plone.vars)
     vars.insert(1, StringVar(
         'title',
         title='Project Title',
@@ -48,6 +51,10 @@ GenericSetup registration for the project and, as such, appears
 in Plone's Add/Remove products form.
 """,
     ))
+    #add_profile should always default to True for dexterity packages
+    get_var(vars, 'add_profile').default = True
+    #add_profile need not appear as a question for dexterity packages
+    get_var(vars, 'add_profile').modes = (EXPERT, )
 
     def pre(self, command, output_dir, vars):
         super(Dexterity, self).pre(command, output_dir, vars)
